c82a28950ac4079772bb1b3cc199bb126864aaae
Nai Williams <naiwilliams97@gmail.com>
2022-02-16 13:03:03 -0500

    Complete redo of js

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-const submit = document.querySelector('button');
-const subscribe = document.querySelector('#email');
-const error = document.querySelector('.error');
-const correct = document.querySelector('.submitted');
+let email = document.querySelector('#email');
+let submit = document.querySelector('button');
+let success = document.querySelector('.submitted');
 
-submit.addEventListener('click', function emailCheck() {
-  if (subscribe.checkValidity() && error.classList.contains('hide')) {
-    correct.classList.remove('hide');
-  } else if (!subscribe.checkValidity() && correct.classList.contains('hide')) {
-    error.classList.remove('hide');
-    subscribe.style.borderColor = 'hsl(354, 100%, 66%)';
+submit.addEventListener('click', function validEmail() {
+  if (email.checkValidity()) {
+    success.classList.replace('hide', 'show');
+    email.style.borderColor = 'hsl(223, 87%, 63%)';
   } else {
-    changeBack();
+    chooseError();
   }
+  messageNum();
 });
 
-function changeBack() {
-  if (subscribe.checkValidity() && !error.classList.contains('hide')) {
-    error.classList.add('hide');
-    subscribe.style.borderColor = 'hsl(223, 87%, 63%)';
-    correct.classList.remove('hide');
-  } else {
-    correct.classList.add('hide');
-    subscribe.style.borderColor = 'hsl(354, 100%, 66%)';
-    error.classList.remove('hide');
+function chooseError() {
+  let empty = document.querySelector('.empty');
+  let invalid = document.querySelector('.invalid');
+
+  if (success.classList.contains('show')) {
+    success.classList.replace('show', 'hide');
+  }
+
+  if (!email.checkValidity() && email.value.length == 0) {
+    empty.classList.replace('hide', 'show');
+    console.log('empty');
+  } else if (!email.checkValidity() && email.value.length > 0) {
+    invalid.classList.replace('hide', 'show');
+  }
+  email.style.borderColor = 'hsl(354, 100%, 66%)';
+}
+
+let messages = document.querySelector('.messages');
+let prevMessage;
+
+function messageNum() {
+  if (messages.getElementsByClassName('show').length > 1) {
+    prevMessage = messages.getElementsByClassName('show')[0];
+    prevMessage.classList.replace('show', 'hide');
   }
 }
